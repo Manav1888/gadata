ed85029adef5c915d62492fba32de3c80caecc1e
Manav1888 <manav@simplifys.com>
2024-04-19 16:15:00 +0530

    mm

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/gadata/',
   plugins: [react()],
-  build: {
-    outDir: 'build', // Set the output directory to 'build'
-    
-  },
+
 })
